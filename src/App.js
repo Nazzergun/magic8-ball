2c792310486c99e0ae9ceffdf505a2f98e5746d0
Naz <72349691+Nazzergun@users.noreply.github.com>
2023-10-02 23:07:25 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [cevap, setCevap] = useState(null);
+  const mesajlar = [
+    "Kesinlikle",
+    " Kesinlikle öyle",
+    " Kuşkusuz",
+    "Belirtiler olduğu yönünde",
+    " Biraz belirsiz, tekrar dene",
+    " Sonra tekrar dene",
+    "Şimdi söylemesem daha iyi",
+    "Şimdi kehanette bulunamam",
+    "Konsantre ol ve tekrar sor",
+    "Bana öyle bakma",
+    "Yanıtım hayır",
+    " Kaynaklarım hayır diyor",
+    "Pek iyi görünmüyor",
+    "Çok şüpheli"
+  ];
+
+  function cevapVer() {
+    const sayi = Math.floor(Math.random() * 13);
+    setCevap(mesajlar[sayi]);
+  }
+
+  return (
+    <div className="App">
+      <h1 contentEditable>Sence yazılımla uğraşmalı mıyım?</h1>
+      <div className="buyuk-top" onClick={cevapVer}>
+        <div className="kucuk-top">
+          {cevap ? <div>{cevap}</div> : <div className="sekiz">8</div>}
+        </div>
+      </div>
+    </div>
+  );
+}
